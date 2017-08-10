0cdbc563896f3593e3edda7673c980cf7aa25dbb
Alok Kuntal <alokkuntal@Aloks-MacBook-Pro.local>
2017-08-10 23:15:18 +0530

    apis

--- server/api/book_api.js
+++ server/api/book_api.js
@@ -1,15 +1,40 @@
 var express = require('express');
 var app = express();
 
-// Import User Module Containing Functions Related To User Data
+// Import Book Module Containing Functions Related To Book Data
 var book = require('../models/book');
 
-app.get('/', function(req, res) {
-
+app.get('/api/books', function(req, res) {
 	book.findAll(function(err, rows, fields) {
 		console.log(err);
 		if(err) throw err;
 		res.json(rows);
 	})
 });
+
+app.post('/api/book/add', function(req, res, next) {
+	var data = req.body;
+	book.addBook(data,function(err, rows, fields){
+		if(err) throw err;
+		res.json(data);
+	})
+});
+
+app.put('/api/book/update', function(req, res, next) {
+	var data = req.body;
+	var condition  = {"id": data.id};	
+	book.updateBook(data, condition, function(err, rows, fields){
+		if(err) throw err;
+		res.json(data);
+	})
+});
+
+app.delete('/api/book/:book_id', function(req, res, next) {
+	var book_id = req.params.book_id;
+	book.deleteBook({"id":book_id}, function(err, rows, fields){
+		if(err) throw err;
+	 	res.json(book_id);
+	})
+});
+
 module.exports = app;
--- server/models/book.js
+++ server/models/book.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "123",
+  password: "123456",
   database: "library"
 });
 
@@ -20,4 +20,16 @@ con.connect(function(err) {
 
 module.exports.findAll = function(callback) {
 	con.query("SELECT * FROM books ORDER BY id DESC", callback);
+}
+
+module.exports.addBook = function(data, callback) {
+  con.query("INSERT INTO books SET ?", data, callback);
+}
+
+module.exports.updateBook = function(data, condition, callback) {
+  con.query("UPDATE books SET ? WHERE ?", [data, condition], callback);
+}
+
+module.exports.deleteBook = function(condition, callback) {
+  con.query("DELETE FROM books WHERE ?", condition, callback);
 }
